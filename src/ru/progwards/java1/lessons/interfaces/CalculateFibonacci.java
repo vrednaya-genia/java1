e8d596c78fdf0f55b423d6cf0543d9841a3542fa
Eugenia <vrednaya.genia@gmail.com>
2020-08-27 21:23:23 +0200

    interfaces

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,27 +1,32 @@
 package ru.progwards.java1.lessons.interfaces;
 
 public class CalculateFibonacci {
-    private static CacheInfo lastFibo = new CacheInfo();
+    private static CacheInfo lastFibo;
 
     // n from 1
     public static int fiboNumber(int n) {
-        if (lastFibo.n==n) {        // при инициализации lastFibo.n и lastFibo.fibo равны 0
-            return lastFibo.fibo;   // поэтому при первом вычислении lastFibo.n==n не может быть true
+        if (lastFibo == null) {
+            lastFibo = new CacheInfo();
         }
 
-        if (n==1) return 1;
-        if (n==2) return 1;
+        if (lastFibo.n==n) {
+            return lastFibo.fibo;
+        }
+
+        if (n==1 || n==2) {
+            lastFibo.n = n;
+            lastFibo.fibo = 1;
+            return 1;
+        }
 
         int fNum = 0;
         int fNum1 = 1;
         int fNum2 = 1;
-
         for (int i = 3; i<=n; i++) {
             fNum = fNum1 + fNum2;
             fNum1 = fNum2;
             fNum2 = fNum;
         }
-
         lastFibo.n = n;
         lastFibo.fibo = fNum;
         return fNum;
@@ -42,8 +47,17 @@ public class CalculateFibonacci {
 
     public static void main(String[] args) {
         System.out.println(fiboNumber(4));
-        System.out.println(fiboNumber(5));
+        System.out.println(fiboNumber(29));
         System.out.println(CalculateFibonacci.lastFibo.n);
         System.out.println(CalculateFibonacci.lastFibo.fibo);
+
+        fiboNumber(1);
+        CacheInfo a = getLastFibo();
+        System.out.println(a.n);
+        System.out.println(a.fibo);
+
+        clearLastFibo();
+
+        fiboNumber(1);
     }
 }
