1cadd638a2bd7cd5364df080b9e4180dd1bde0f5
Eugenia <vrednaya.genia@gmail.com>
2020-09-13 03:18:32 +0200

    io2

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -1,11 +1,88 @@
 package ru.progwards.java1.lessons.io2;
 
-public class Censor {
-    public static void censorFile(String inoutFileName, String[] obscene) {
+import java.io.FileWriter;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
 
+public class Censor {
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException, Exception {
+        File fn = new File(inoutFileName);
+        try (Scanner scanner = new Scanner(fn)) {
+            RandomAccessFile raf = new RandomAccessFile(fn, "rw");
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                String repl = "";
+                boolean wr = true;
+                // для сравнения выделяем слово без знаков препинания
+                String word2 = "";
+                String prew = ""; // знаки препинания до слова
+                String postw = ""; // знаки препинания после слова
+                for (char c : word.toCharArray())
+                    if (Character.isAlphabetic(c)) {
+                        word2 = word2 + c;
+                    } else if ("".equals(word2)) {
+                        prew = prew + c;
+                    } else {
+                        postw = postw + c;
+                    }
+
+                for (int i = 0; i < obscene.length; i++) {
+                    if (word2.equals(obscene[i])) {
+                        for (int j = 0; j < obscene[i].length(); j++) {
+                            repl = repl + "*";
+                        }
+                        repl = prew + repl + postw + " ";
+                        raf.write(repl.getBytes());
+                        wr = false;
+                        break;
+                    }
+                }
+                if (wr) {
+                    word = word + " "; // записываем со знаками препинания
+                    raf.write(word.getBytes());
+                }
+            }
+        } catch (Exception e) {
+            throw new CensorException(e.getMessage(), inoutFileName);
+        }
     }
 
-    class CensorException {
-        
+    static class CensorException extends Throwable implements AutoCloseable {
+        String mExc;
+        String fName;
+
+        CensorException(String mExc, String fName) throws Exception {
+            this.mExc = mExc;
+            this.fName = fName;
+        }
+
+        @Override
+        public void close() throws Exception {
+            throw new Exception();
+        }
+
+        @Override
+        public String toString() {
+            return "<"+fName+">:<"+mExc+">";
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("D:\\123.txt");
+            writer.write("Java — язык программирования, " +
+                    "разработанный Sun Microsystems " +
+                    "(в последующем приобретённой Oracle). ");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        String[] obscene = {"Java", "Oracle", "Sun", "Microsystems"};
+        try {
+            censorFile("D:\\123.txt", obscene);
+        } catch (Exception | CensorException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/ru/progwards/java1/lessons/io2/Translator.java
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -5,11 +5,48 @@ public class Translator {
     private String[] outLang;
 
     Translator(String[] inLang, String[] outLang) {
-        //
-
+        this.inLang = inLang;
+        this.outLang = outLang;
     }
 
     public String translate(String sentence) {
-        return "";
+        String res = "";
+        String[] words = sentence.split("\\s");
+        for (int i=0; i<words.length; i++) {
+            boolean isUp = Character.isUpperCase(words[i].charAt(0));
+            // для поиска выделяем слово без знаков препинания
+            // и переводим в нижний регистр
+            String word2 = "";
+            String prew = ""; // знаки препинания до слова
+            String postw = ""; // знаки препинания после слова
+            for (char c : words[i].toCharArray())
+                if (Character.isAlphabetic(c)) {
+                    word2 = word2 + Character.toLowerCase(c);
+                } else if ("".equals(word2)) {
+                    prew = prew + c;
+                } else {
+                    postw = postw + c;
+                } // слова с дефизом %)
+
+            for (int j=0; j<inLang.length; j++) {
+                if (word2.equals(inLang[j])) {
+                    if (isUp) {
+                        word2 = outLang[j].substring(0,1).toUpperCase()+outLang[j].substring(1);
+                    }
+                    res = res + prew + word2 + postw + " ";
+                    break;
+                }
+            }
+        }
+
+        res = res.substring(0,res.length()-1);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String[] ain = {"hello", "world"};
+        String[] aout = {"привет", "мир"};
+        Translator aa = new Translator(ain, aout);
+        System.out.println(aa.translate("Hello World!"));
     }
 }
