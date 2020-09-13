79fed9262f492cb0bdc8484fb990e7d2ef75eff6
Eugenia <vrednaya.genia@gmail.com>
2020-09-13 19:09:08 +0200

    io2

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -23,47 +23,51 @@ public class Censor {
     }
 
     public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
-        File fn = new File(inoutFileName);
-        try (Scanner scan = new Scanner(fn)) { // воспринимал строку пути как просто строку %)
-            RandomAccessFile raf = new RandomAccessFile(fn, "rw");
-            while (scan.hasNext()) {
-                String word = scan.next();
-                String repl = "";
-                boolean wr = true;
-                // для сравнения выделяем слово без знаков препинания
-                String word2 = "";
-                String prew = ""; // знаки препинания до слова
-                String postw = ""; // знаки препинания после слова
-                for (char c : word.toCharArray())
-                    if (Character.isAlphabetic(c)) {
-                        word2 = word2 + c;
-                    } else if ("".equals(word2)) {
-                        prew = prew + c;
-                    } else {
-                        postw = postw + c;
-                    } // слова через дефиз?
+        try {
+            File fn = new File(inoutFileName);
+            try (Scanner scan = new Scanner(fn)) { // воспринимал строку пути как просто строку %)
+                RandomAccessFile raf = new RandomAccessFile(fn, "rw");
+                while (scan.hasNext()) {
+                    String word = scan.next();
+                    String repl = "";
+                    boolean wr = true;
+                    // для сравнения выделяем слово без знаков препинания
+                    String word2 = "";
+                    String prew = ""; // знаки препинания до слова
+                    String postw = ""; // знаки препинания после слова
+                    for (char c : word.toCharArray())
+                        if (Character.isAlphabetic(c)) {
+                            word2 = word2 + c;
+                        } else if ("".equals(word2)) {
+                            prew = prew + c;
+                        } else {
+                            postw = postw + c;
+                        } // слова через дефиз?
 
-                for (int i = 0; i < obscene.length; i++) {
-                    if (word2.equals(obscene[i])) {
-                        for (int j = 0; j < obscene[i].length(); j++) {
-                            repl = repl + "*";
+                    for (int i = 0; i < obscene.length; i++) {
+                        if (word2.equals(obscene[i])) {
+                            for (int j = 0; j < obscene[i].length(); j++) {
+                                repl = repl + "*";
+                            }
+                            if (scan.hasNext()) {
+                                repl = prew + repl + postw + " ";
+                            }
+                            raf.write(repl.getBytes());
+                            wr = false;
+                            break;
                         }
+                    }
+                    if (wr) {
                         if (scan.hasNext()) {
-                            repl = prew + repl + postw + " ";
+                            word = word + " ";
                         }
-                        raf.write(repl.getBytes());
-                        wr = false;
-                        break;
+                        raf.write(word.getBytes());
                     }
                 }
-                if (wr) {
-                    if (scan.hasNext()) {
-                        word = word + " ";
-                    }
-                    raf.write(word.getBytes());
-                }
+                raf.close();
+            } catch (Exception e) {
+                throw new CensorException(e.getMessage(), inoutFileName);
             }
-            raf.close();
         } catch (Exception e) {
             throw new CensorException(e.getMessage(), inoutFileName);
         }
