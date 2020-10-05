c50ef8cdf4528777c80fecd91c594e65d2c4346b
Eugenia <vrednaya.genia@gmail.com>
2020-10-05 19:01:32 +0200

    map

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -9,52 +9,37 @@ public class FiboMapCache {
     boolean isOn;
 
     public FiboMapCache(boolean cacheOn) {
-        if (cacheOn) {
-            isOn = true;
-        } else {
-            isOn = false;
-        }
+        isOn = cacheOn;
     }
 
     public BigDecimal fiboNumber(int n) {
         if (isOn) {                // cacheOn true
-            // но кеш не используется
-            if (fiboCache == null) {
+            if (fiboCache==null) {
                 fiboCache = new HashMap();
-                if (n==1 || n==2) {
-                    fiboCache.put(n, BigDecimal.ONE);
-                    return BigDecimal.ONE;
-                }
-                long fNum = fiboNum(n);
-                fiboCache.put(n, BigDecimal.valueOf(fNum));
-                return BigDecimal.valueOf(fNum);
             }
-            // кеш запоминается
-            //if (fiboCache.containsKey(n)) {
-            //    return fiboCache.get(n);
-            //}
+            if (fiboCache.containsKey(n)) {
+                return fiboCache.get(n);
+            }
             if (n==1 || n==2) {
                 fiboCache.put(n, BigDecimal.ONE);
                 return BigDecimal.ONE;
             }
-            BigDecimal num = fiboCache.get(n-1).add(fiboCache.get(n-2));
-            fiboCache.put(n, num);
-            return num;
+            fiboCache.put(n, fiboNum(n));
         } else {                             // cacheOn false
             if (n==1 || n==2) {
                 return BigDecimal.ONE;
             }
-            long fNum = fiboNum(n);
-            return BigDecimal.valueOf(fNum);
         }
+
+        return fiboNum(n);
     }
 
-    public static long fiboNum(int n) {
-        long fNum = 0;
-        long fNum1 = 1;
-        long fNum2 = 1;
+    public static BigDecimal fiboNum(int n) {
+        BigDecimal fNum = BigDecimal.ZERO;
+        BigDecimal fNum1 = BigDecimal.ONE;
+        BigDecimal fNum2 = BigDecimal.ONE;
         for (int i = 3; i<=n; i++) {
-            fNum = fNum1 + fNum2;
+            fNum = fNum1.add(fNum2);
             fNum1 = fNum2;
             fNum2 = fNum;
         }
@@ -70,7 +55,7 @@ public class FiboMapCache {
         long start = System.currentTimeMillis();
         for (int i=1; i<=1000; i++) {
             test1.fiboNumber(i);
-            //test1.clearCahe();
+            test1.clearCahe();
         }
         long end = System.currentTimeMillis()-start;
         System.out.println("fiboNumber cacheOn=true время выполнения " + end);
